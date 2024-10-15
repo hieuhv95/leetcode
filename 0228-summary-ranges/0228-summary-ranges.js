a081225d5bc1e1395fb7f06aba575fed8574cb50
hieuhv95 <hvhieu95@gmail.com>
2024-10-16 01:02:19 +0900

    Time: 53 ms (41.62%), Space: 48.9 MB (40.21%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.js
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -6,20 +6,23 @@ var summaryRanges = function (nums) {
      let ranges = [];
     if (nums.length === 0) return ranges; 
     let startNum = nums[0];
+    let endNum;
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] !== nums[i - 1] + 1) {
-            if (startNum === nums[i - 1]) {
+             endNum = nums[i - 1];
+            if (startNum === endNum) {
                 ranges.push(startNum.toString());
             } else {
-                ranges.push(startNum + "->" + nums[i - 1]);
+                ranges.push(startNum + "->" + endNum);
             }
             startNum = nums[i]; 
         }
     }
-    if (startNum === nums[nums.length - 1]) {
+    endNum = nums[nums.length - 1];
+    if (startNum === endNum) {
         ranges.push(startNum.toString());
     } else {
-        ranges.push(startNum + "->" + nums[nums.length - 1]);
+        ranges.push(startNum + "->" + endNum);
     }
     return ranges; 
     };
